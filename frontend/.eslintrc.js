b36d71f36a3e9dc376659cea0865da06ea43059f
kmasuda <43663858+kmasuda1986@users.noreply.github.com>
2023-04-15 15:30:47 +0900

    fix: Refactor (#5)

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -5,30 +5,29 @@ module.exports = {
     node: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react", "@typescript-eslint"],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
 };
-
--- frontend/pages/Mypage.tsx
+++ frontend/pages/Mypage.tsx
@@ -3,49 +3,50 @@ import { Button, Text, Input, Grid } from '@nextui-org/react';
 import { Nav } from '../components/navbar/Nav';
 
 export default function Mypage() {
-
   return (
     <>
-        <Nav />
+      <Nav />
 
-        <Grid.Container gap={2} css={{ margin: "5" }}>
-
-            <Grid xs={12} justify="center">
-                <Text h1 color='white'>MyPage</Text>
-            </Grid>
-            <Grid xs={12} justify='center'>
-                <Grid xs={4} justify='center'>
-                    <Input 
-                        label="Name"
-                        clearable
-                        bordered
-                        fullWidth
-                        color='primary'
-                        size="lg"
-                        placeholder='Enter building name'
-                        contentLeft={<Text>🏢</Text>}
-                    />
-                </Grid>
-            </Grid>
-            <Grid xs={12} justify='center'>
-                <Grid xs={4} justify='center'>
-                    <Input 
-                        label="Address"
-                        clearable
-                        bordered
-                        fullWidth
-                        color='primary'
-                        size="lg"
-                        placeholder='Enter building address'
-                        contentLeft={<Text>📍</Text>}
-                    />
-                </Grid>
-            </Grid>
-            <Grid xs={12} justify='center'>
-                <Button color="gradient" shadow>sell</Button>
-            </Grid>
-            
-        </Grid.Container>
+      <Grid.Container gap={2} css={{ margin: '5' }}>
+        <Grid xs={12} justify="center">
+          <Text h1 color="white">
+            MyPage
+          </Text>
+        </Grid>
+        <Grid xs={12} justify="center">
+          <Grid xs={4} justify="center">
+            <Input
+              label="Name"
+              clearable
+              bordered
+              fullWidth
+              color="primary"
+              size="lg"
+              placeholder="Enter building name"
+              contentLeft={<Text>🏢</Text>}
+            />
+          </Grid>
+        </Grid>
+        <Grid xs={12} justify="center">
+          <Grid xs={4} justify="center">
+            <Input
+              label="Address"
+              clearable
+              bordered
+              fullWidth
+              color="primary"
+              size="lg"
+              placeholder="Enter building address"
+              contentLeft={<Text>📍</Text>}
+            />
+          </Grid>
+        </Grid>
+        <Grid xs={12} justify="center">
+          <Button color="gradient" shadow>
+            sell
+          </Button>
+        </Grid>
+      </Grid.Container>
     </>
-  )
+  );
 }
